54d29aaed90bece605dcc6b4437ae9fdd0bd0534
andreabrunoxy <andreabrunoxy@users.noreply.github.com>
2021-03-02 19:45:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ProgressBar from "./components/ProgressBar";
+import React, { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [mistakes, setMistakes] = useState(0);
+  const [currentProblem, setCurrentProblem] = useState(generateProblem());
+  const [userAnswer, setUserAnswer] = useState("");
+  const [showError, setShowError] = useState(false);
+  const answerField = useRef(null);
+  const resetButton = useRef(null);
+
+  useEffect(() => {
+    if (score == 10 || mistakes == 3) {
+      setTimeout(() => resetButton.current.focus(), 331);
+    }
+  }, [score, mistakes]);
+
+  function generateNumber(max) {
+    return Math.floor(Math.random() * (max + 1));
+  }
+
+  function generateProblem() {
+    return {
+      numberOne: generateNumber(10),
+      numberTwo: generateNumber(10),
+      operator: ["+", "-", "x"][generateNumber(2)]
+    };
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    answerField.current.focus();
+
+    let correctAnswer;
+    if (currentProblem.operator == "+") correctAnswer = currentProblem.numberOne + currentProblem.numberTwo;
+    if (currentProblem.operator == "-") correctAnswer = currentProblem.numberOne - currentProblem.numberTwo;
+    if (currentProblem.operator == "x") correctAnswer = currentProblem.numberOne * currentProblem.numberTwo;
+
+    if (correctAnswer == parseInt(userAnswer, 10)) {
+      setScore(prev => prev + 1);
+      setCurrentProblem(generateProblem());
+      setUserAnswer("");
+    } else {
+      setMistakes(prev => prev + 1);
+      setShowError(true);
+      setTimeout(() => setShowError(false), 401);
+    }
+  }
+
+  function resetGame() {
+    setScore(0);
+    setMistakes(0);
+    setUserAnswer("");
+    setCurrentProblem(generateProblem());
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <div className={"main-ui" + (mistakes == 3 || score == 10 ? " blurred" : "")}>
+        <p className={"problem" + (showError ? " animate-wrong" : "")}>
+          {currentProblem.numberOne} {currentProblem.operator} {currentProblem.numberTwo}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <form onSubmit={handleSubmit} action="" className="our-form">
+          <input ref={answerField} value={userAnswer} onChange={e => setUserAnswer(e.target.value)} type="text" className="our-field" autoComplete="off" />
+          <button>Submit</button>
+        </form>
+
+        <p className="status">
+          You need {score} more points, and are allowed to make {2 - mistakes} more mistakes.
+        </p>
+        <ProgressBar score={score} />
+      </div>
+
+      <div className={"overlay" + (mistakes == 3 || score == 10 ? " overlay--visible" : "")}>
+        <div className="overlay-inner">
+          <p className="end-message">{score == 10 ? "Congrats, you won!" : "Sorry, you lost!"}</p>
+          <button ref={resetButton} onClick={resetGame} className="reset-button">
+            Start Over
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
